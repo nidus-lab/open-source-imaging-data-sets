70a2bbc523151e830eceb6a2e7f254f7f1e3c7c2
Sharpz7 <adam.mcarthur62@gmail.com>
2025-08-29 01:22:39 +0000

    Trying again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
    trailingSlash: true,
    images: {
       unoptimized: true
--- public/redirect.js
+++ public/redirect.js
@@ -1,5 +1,7 @@
 (function () {
-    const basePath = "/open-source-imaging-data-sets";
+    // Try to infer base path dynamically for project pages
+    const pathParts = window.location.pathname.split('/').filter(Boolean)
+    const basePath = pathParts.length > 0 ? `/${pathParts[0]}` : '';
     const currentPath = window.location.pathname;
 
     // Remove the base path from the current path to get the actual route
